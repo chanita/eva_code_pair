08e1748cb1d80b92e7ac3768aafdd99a8eea3ee9
Nphsst <nphsst_eva@odds.team>
2024-04-15 16:38:37 +0700

    add array assignment

--- array/array_assignment.js
+++ array/array_assignment.js
@@ -5,28 +5,15 @@
 //• คำนวณราคาสินค้า เมื่อรวม tax แล้ว ของ 3 ชิ้น
 
 // เก็บราคาสินค้า เป็น array 3ชิ้น เก็บเป็น number
-const groceryPriceList = [30,52,16.5,10];
+const groceryPriceList = [30, 52, 16.5];
 
 //สร้าง function ชื่อ calTax รับ parameter priceBeforeTax
 //function การหา tax ของสินค้า
 function calTax(priceBeforeTax) {
-    return priceBeforeTax * 7 / 100;
+    return (priceBeforeTax * 7 / 100) + priceBeforeTax;
 }
 
 //เก็บตัวแปร ชื่อ tax และใช้ method ของ array .map เรียก calTax function เพื่อหา tax ของแต่ละสินค้า ทั้งหมดใน array
 const tax = groceryPriceList.map(calTax);
-console.log(tax);
 
-//สร้าง function ใหม่ ชื่อ sumPriceAndTax เพื่อหาราคาหลังรวม Tax
-//function ทำงานโดย รับ parameter 2 ตัว (1 รับ tax, 2 รับ index ของ groceryPriceList)
-//เพื่อหา ผลรวมของ tax และ ราคาก่อน tax
-function sumPriceAndTax(tax,index) {
-    return tax + groceryPriceList[index];
-}
-
-//เก็บ ตัวแปร ชื่อ actualPrice และใช้ method ของ array .map เรียก sumPriceAndTax function เพื่อคำนวนราคา หลังรวม tax ทั้งหมดที่มีใน array
-const actualPrice = tax.map(sumPriceAndTax);
-console.log(actualPrice);
-
-
-module.exports = calTax;
+module.exports = { calTax, tax };
--- array/array_assignment.test.js
+++ array/array_assignment.test.js
@@ -1,6 +1,11 @@
-const calTax = require("./array_assignment.js");
+const { calTax, tax } = require("./array_assignment.js");
 
 test("Calculate tax", () => {
     const tellTaxPrice = calTax(10);
-    expect(tellTaxPrice).toBe(0.7);
-})
+    expect(tellTaxPrice).toBe(10.7);
+});
+
+test("Calculate all tax", () => {
+    const tellTaxPrice = tax;
+    expect(tellTaxPrice).toEqual([32.1, 55.64, 17.655]);
+});
